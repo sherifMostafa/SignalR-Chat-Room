705852fc595a3296d9a652db09fc2c4bad500ae8
Sherif Mostafa <sherifmostafa94@outlook.com>
2021-08-21 17:32:55 +0200

    Add project files.

---
+++ SignalR/ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalR
+{
+    [HubName("chat")]
+    public class ChatHub : Hub
+    {
+        public void sendMessage(string name, string message)
+        {
+
+            // save db
+
+
+            Clients.All.newMessage(name , message);
+        }
+    }
+}
